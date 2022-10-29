b225177432a0ec9e54217486dc67f2e4b230f847
SergioCaueGomes <caue.sergio@gmail.com>
2022-10-29 20:26:33 -0300

    Commit Criando Classes

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição Curso JavaScript");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+    }
+}
